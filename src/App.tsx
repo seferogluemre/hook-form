46b52d75a4230346aa4c7a04a2e9403758f5edd3
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-07 10:25:09 +0300

    zot paketinin kurulumu ve yup kullanımı

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-import { useForm, SubmitHandler } from "react-hook-form"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { z } from 'zot'
 
-interface Inputs {
-  firstName: string
-  lastName: string
-}
+
+const schema = z
+  .object({
+    firstName: z.string(),
+    age: z.number().positive(),
+  })
+
+type Inputs = z.infer<typeof schema>;
 
 export default function App() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>()
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  })
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
-  const exampleValue = watch("firstName")
-
-  console.log(exampleValue) // watch input value by passing the name of it
-
-  console.log(errors.lastName)
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("firstName")} />
+      <input {...register("firstName")} />
+      <p>{errors.firstName?.message}</p>
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("lastName", { required: true, minLength: 5 })} />
-      {/* errors will return when field validation fails  */}
-      {errors.lastName?.type == "required" && <span>Bu alan zorunlu</span>}
-      {errors.lastName?.type == "minLength" && <span>En az 5 karakter girilebilşr</span>}
+
+      <input {...register("age")} />
+      <p>{errors.age?.message}</p>
+
 
       <input type="submit" />
     </form>
