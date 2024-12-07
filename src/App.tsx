e0b7b1d4c991aa5db498324174e1202a7e9ba9b7
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-07 09:56:12 +0300

    form validasyonlarının eklenmesi ve yönetimin saglanması

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import './App.css'
+import { useForm, SubmitHandler } from "react-hook-form"
 
-function App() {
-
-  return (
-    <>
-      emre
-    </>
-  )
+interface Inputs {
+  firstName: string
+  lastName: string
 }
 
-export default App
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  const exampleValue = watch("firstName")
+
+  console.log(exampleValue) // watch input value by passing the name of it
+
+  console.log(errors.lastName)
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("firstName")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("lastName", { required: true, minLength: 5 })} />
+      {/* errors will return when field validation fails  */}
+      {errors.lastName?.type == "required" && <span>Bu alan zorunlu</span>}
+      {errors.lastName?.type == "minLength" && <span>En az 5 karakter girilebilşr</span>}
+
+      <input type="submit" />
+    </form>
+  )
+}
